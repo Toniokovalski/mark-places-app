9159d5346ee498bc48788c2ee6c74f09377237a6
Anton Koval <toniokovalski@gmail.com>
2023-09-04 16:37:39 +0200

    Remove unnecessary code
    Remove unnecessary code

--- src/App.jsx
+++ src/App.jsx
@@ -11,22 +11,12 @@ import City from "./components/City/City";
 import Form from "./components/Form/Form";
 import SpinnerFullPage from "./components/Spinner/SpinnerFullPage";
 
-// import Product from "./pages/Product";
-// import Pricing from "./pages/Pricing";
-// import Homepage from "./pages/Homepage";
-// import Login from "./pages/Login";
-// import AppLayout from "./pages/AppLayout";
-// import PageNotFound from "./pages/PageNotFound";
-
 const Homepage = lazy(() => import("./pages/Homepage/Homepage"));
 const Product = lazy(() => import("./pages/Product/Product"));
 const Pricing = lazy(() => import("./pages/Pricing/Pricing"));
 const Login = lazy(() => import("./pages/Login/Login"));
 const AppLayout = lazy(() => import("./pages/AppLayout/AppLayout"));
 const PageNotFound = lazy(() => import("./pages/PageNotFound/PageNotFound"));
-
-// dist/assets/index-59fcab9b.css   30.56 kB │ gzip:   5.14 kB
-// dist/assets/index-f7c12d89.js   572.44 kB │ gzip: 151.29 kB
 
 function App() {
   return (
